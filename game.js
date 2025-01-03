91d257ffddd2e756c2d810614da677af3062d5d5
amarshri <amarshri6139@gmail.com>
2025-01-03 17:05:17 +0530

    start the project

---
+++ game.js
@@ -0,0 +1,17 @@
+let boxes=document.querySelector(".box");
+let reset=document.querySelector("#reset");
+
+let turn0 = true;
+// 2d array
+
+const win={
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+}
+
